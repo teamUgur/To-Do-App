f9cf3f23adc6ad6327972594fe25eaa66b284568
Kubinsky <166978514+teamUgur@users.noreply.github.com>
2026-06-18 20:37:15 +0300

    read and create

--- main.py
+++ main.py
@@ -21,3 +21,23 @@ def get_db():
         yield db
     finally:
         db.close()
+
+@app.post('/items/', respomse_model=schemas.Item)
+def create_item(item: schemas.CreateItem, db: Depends(get_db)):
+    item_id = models.Item(**item.dict())
+    db.add(item_id)
+    db.commit()
+    db.refresh(item_id)
+    return item_id
+
+@app.get('/items/', response_model=list[schemas.Item])
+def read_all_items(skip: 0, limit: 100, db: Depends(get_db)):
+    items = db.query(models.Item).offset(skip).limit(limit).all()
+    return items
+
+@app.get('/items/{item_id}', response_model=schemas.Item)
+def read_item(item_id: int, db: Depends(get_db)):
+    item = db.query(models.Item).filter(item_id == models.Item.id).first()
+    if item == None:
+        raise HTTPException(status_code=404, detail='Item not found')
+    return item
