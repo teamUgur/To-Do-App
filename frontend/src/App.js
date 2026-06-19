5805059c63fcf2bdb86cddbe2cdd393b90196e0a
Kubinsky <166978514+teamUgur@users.noreply.github.com>
2026-06-19 15:46:20 +0300

    fetch items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const API_BASE = 'http://localhost:8000';
+
 function App() {
+
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [editingId, setEditingId] = useState(null);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    try {
+      const response = axios.get(`${API_BASE}/items/`);
+      setItems(response.data);
+    } catch (error) {
+      console.error("Error fetching items:", error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
