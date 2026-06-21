6f9fcd8f3babbf50ed4967cb88850db831f0b800
Kubinsky <166978514+teamUgur@users.noreply.github.com>
2026-06-21 17:04:38 +0300

    models new

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,19 @@
-from typing import Optional
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    hashed_password: Mapped[str] = mapped_column()
 
 class Item(Base):
     __tablename__ = "items"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(index=True)
-    description: Mapped[Optional[str]] = mapped_column(index=True, default=None)
+    description: Mapped[str | None] = mapped_column(index=True, default=None)
+
+    owner_id: Mapped[int] = mapped_column(FoeignKey("users.id"))
     
