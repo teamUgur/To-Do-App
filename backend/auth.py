8350725fdfd8b34a4941fbf5ce2ef1e9e97f44e0
Kubinsky <166978514+teamUgur@users.noreply.github.com>
2026-06-22 14:38:34 +0300

    new def

--- backend/auth.py
+++ backend/auth.py
@@ -1,10 +1,10 @@
 import os
-from datetime import datetie, timezone, timedelta
+from datetime import datetime, timezone, timedelta
 from typing import Annotated
 
 from fastapi import Router, status, HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from jose import JWSError, jwt
+from jose import JWTError, jwt
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from passlib.context import CryptContext
@@ -31,3 +31,35 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
+def authenticate_user(username: str, password: str, db):
+    query = select(User).where(User.username == username)
+    user = db.scalar(query)
+
+    if not user:
+        return False
+    if not crypt_context (password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(username: str, user_id: int, expire_time: timedelta):
+    encode = {'sub': username, 'id': user_id}
+    expires = datetime.now(timezone.utc) + timedelta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def get_current_user(token: Annotated(str, Depends(password_bearer))):
+    try: 
+        payload = jwt.decode(token,SECRET_KEY, algorithm=[ALGORITHM])
+        username: str | None = payload.get('sub')
+        user_id: int | None = payload.get('id')
+
+        if not username or not user_id:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not verify the user"
+            )
+    except JWTError:
+        raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not verify the user"
+            )
