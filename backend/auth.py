95f03be0afdc8c820af410531d018909d08d1b7e
Kubinsky <166978514+teamUgur@users.noreply.github.com>
2026-06-23 15:57:19 +0300

    routers added

--- backend/auth.py
+++ backend/auth.py
@@ -63,3 +63,23 @@ def get_current_user(token: Annotated(str, Depends(password_bearer))):
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Could not verify the user"
             )
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    create_user_request = User (
+        username=create_user_request.username,
+        hashed_password=bcrypt_context.hash(create_user_request.password)
+    )
+    db.add(create_user_request)
+    db.commit()
+
+@router.post('/token', response_model=Token)
+async def login_access_token(db: db_dependency, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status.HTTP_401_UNAUTHORIZED,
+            detail="Couldn't validate the user"
+        )
+    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    return {"token": token, "token_type": bearer}
